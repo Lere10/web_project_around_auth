3a087c8a2424e91304187e1e2994ee2dc73a7ec7
Lere <leredsb10@gmail.com>
2025-01-20 21:21:20 -0300

    protectedRoute

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,52 @@
-import { StrictMode } from "react";
+import { React, StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
 import "./index.css";
 import App from "./components/App.jsx";
 import Login from "./components/Login/Login.jsx";
 import Register from "./components/Register/Register.jsx";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute.jsx";
+
+const navigate = useNavigate();
+const location = useLocation();
+const [userData, setUserData] = useState({ username: "", email: "" });
+const [isLoggedIn, setIsLoggedIn] = useState(false);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/signin" element={<Login />} />
-        <Route path="/signup" element={<Register />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute isLoggedIn={isLoggedIn}>
+              <App setIsLoggedIn={setIsLoggedIn} />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/signin"
+          element={
+            <ProtectedRoute isLoggedIn={isLoggedIn} anonymous>
+              <Login />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <ProtectedRoute isLoggedIn={isLoggedIn} anonymous>
+              <Register />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   </StrictMode>
